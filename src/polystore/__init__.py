8a25edf90749a2e80aeb179082ec10820c7c88bc
Tristan Simas <tristan.simas@mail.mcgill.ca>
2025-11-02 00:08:14 -0400

    fix(deps): replace git dependency with PyPI package for metaclass-registry
    - Change metaclass-registry from git+https://github.com/trissim/metaclass-registry.git@main to >=0.1.0
    - Bump version to 0.1.2
    - Fixes PyPI publish failure: PyPI doesn't allow direct git dependencies

--- src/polystore/__init__.py
+++ src/polystore/__init__.py
@@ -5,7 +5,7 @@ Provides pluggable storage backends with multi-framework I/O support for
 NumPy, PyTorch, JAX, TensorFlow, CuPy, and Zarr.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Core abstractions
 from .base import (
