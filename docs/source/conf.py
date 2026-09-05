530d87ac0b6a5e14de1cee998b03ab267900b30b
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-09-04 21:52:00 -0400

    Remove unused SciPy documentation inventory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -127,7 +127,6 @@ napoleon_attr_annotations = True
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
     'matplotlib': ('https://matplotlib.org/stable/', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
     'scikit-image': ('https://scikit-image.org/docs/stable/', None),
